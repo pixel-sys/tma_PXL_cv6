e2524f74c8c0a593f6f23f5d19d61ee9e6191354
pixel-sys <1xawer1@gmail.com>
2022-04-03 13:18:43 +0200

    Cv6 Uloha C+D - WEBKIT open new activity with WEBKIT and open web with Github repo

--- app/src/main/java/com/example/tma_pxl/Napoveda.java
+++ app/src/main/java/com/example/tma_pxl/Napoveda.java
@@ -1,7 +1,7 @@
+/*Xaver Zak*/
 package com.example.tma_pxl;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
